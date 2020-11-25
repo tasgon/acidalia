0d49383236515af2ea706a2575c1858a8f6d9d6b
telastrus <10052313+telastrus@users.noreply.github.com>
2020-11-25 03:42:50 -0500

    a lot of stuff cleaned up

--- src/engine.rs
+++ src/engine.rs
@@ -1,25 +1,50 @@
+use std::sync::Arc;
+
 use iced_winit::winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
 };
 
+use crate::graphics::GraphicsState;
+
 pub struct Engine {
-    event_loop: EventLoop<()>,
+    event_loop: Option<EventLoop<()>>,
     window: Window,
+    pub graphics_state: GraphicsState,
+    screens: Vec<Box<dyn Screen>>,
 }
 
 impl Engine {
     pub fn new() -> Self {
         let event_loop = EventLoop::new();
         let window = WindowBuilder::new().build(&event_loop).unwrap();
+        let graphics_state = GraphicsState::new(&window);
         Self {
-            event_loop,
+            event_loop: Some(event_loop),
             window,
+            graphics_state,
+            screens: vec![],
         }
     }
 
-    pub fn run(&mut self) {
-
+    pub fn add_screen(&mut self, screen: impl Into<Box<dyn Screen>>) {
+        //self.screens.push(screen.into());
     }
+
+    pub fn run(mut self) {
+        let evloop = self.event_loop.take().unwrap();
+        let id = self.window.id();
+        evloop.run(move |event, _, control_flow| {
+            if let Some(screen) = { self.screens.last_mut() } {
+                screen.update(event);
+                screen.render(&self.graphics_state);
+            }
+        });
+    }
+}
+
+pub trait Screen {
+    fn update(&mut self, event: Event<()>);
+    fn render(&self, engine: &GraphicsState);
 }
