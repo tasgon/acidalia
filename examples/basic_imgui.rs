f4b9d8a68536bca5235cf8cbdac0528ddcfc8a5f
tasgon <tasgon_@outlook.com>
2021-03-09 04:47:57 -0500

    added basic imgui example

---
+++ examples/basic_imgui.rs
@@ -0,0 +1,34 @@
+use acidalia::ui::{imgui, ImguiElement};
+use acidalia::{screen, EngineBuilder};
+use imgui::im_str;
+
+#[derive(Default)]
+struct Data {
+    count: u32,
+}
+
+fn main() {
+    let engine = EngineBuilder::new(|wb| wb.with_maximized(true))
+        .bg_color(acidalia::wgpu::Color {
+            r: 0.1,
+            g: 0.2,
+            b: 0.3,
+            a: 1.0,
+        })
+        .build();
+    let ui_el = ImguiElement::new(
+        |ui, d: &mut Data| {
+            imgui::Window::new(im_str!("Main")).build(ui, || {
+                if ui.small_button(im_str!("Increment count")) {
+                    d.count += 1;
+                }
+
+                ui.text(im_str!("Count: {}", d.count));
+            });
+        },
+        &engine,
+    );
+
+    let data = Data::default();
+    engine.run(screen!(ui_el), data);
+}
